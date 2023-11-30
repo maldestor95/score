145c357446bd93824995a11a15a6be764ed94aa9
maldestor95 <maldestor95@gmail.com>
2023-11-30 09:39:19 +0100

    update about pages

--- src/components/pdf.vue
+++ src/components/pdf.vue
@@ -1,7 +1,28 @@
 <template>
-  <div></div>
+  <ul class="text-start container px-2">
+    <li class="flex flex-row">
+      <img src="/flavico.svg" class="w-6 mr-2" />
+      <a :href="dorfEnUrl">English Rules</a>
+    </li>
+    <li class="flex flex-row">
+      <img src="/flavico.svg" class="w-6 mr-2" />
+      <a :href="dorfFrUrl">Règles en français</a>
+    </li>
+  </ul>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const dorfEnUrl = ref(
+  "https://raw.githubusercontent.com/maldestor95/dorf/master/public/Dorfromantik.pdf"
+);
+const dorfFrUrl = ref(
+  "https://raw.githubusercontent.com/maldestor95/dorf/master/public/DorfromantikFR.pdf"
+);
+</script>
 
-<style scoped></style>
+<style scoped>
+li {
+  margin-bottom: 0.1rem;
+}
+</style>
