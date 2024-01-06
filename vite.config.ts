a54e62530ffe8f29ddb04794f2cc27dc1eed722b
maldestor95 <maldestor95@gmail.com>
2024-01-06 14:06:36 +0100

    chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: "",
+  define: {
+    // enable hydration mismatch details in production build
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+  }
 })
