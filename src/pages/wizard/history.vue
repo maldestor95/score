2cc16e30a3a7e9cf57d95bdf06873e55ea2391f6
maldestor95 <maldestor95@gmail.com>
2024-01-14 12:39:38 +0100

    firsthistory

---
+++ src/pages/wizard/history.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>History</div>
+  <container class="flex flex-row justify-evenly">
+    <div class="w-20 font-semibold">
+      <div>Round</div>
+      <div v-for="(item, index) in store.getRoundNumber" :key="index">
+        {{ item }}
+      </div>
+    </div>
+    <div v-for="(player, playerId) in store.userList" :key="playerId" class="w-40">
+      <div class="font-semibold">{{ player.name }}</div>
+      <div v-for="roundIScore in player.scorePerRound">
+        {{ roundIScore }}
+      </div>
+    </div>
+  </container>
+</template>
+
+<script setup lang="ts">
+import { useScoreStore } from "./store";
+import container from "../../components/container.vue";
+const store = useScoreStore();
+</script>
+
+<style scoped></style>
