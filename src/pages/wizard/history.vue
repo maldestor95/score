edb6792d80a584a48b44a70022d2f32749432577
maldestor95 <maldestor95@gmail.com>
2024-01-15 06:31:10 +0100

    fix scroll

--- src/pages/wizard/history.vue
+++ src/pages/wizard/history.vue
@@ -1,16 +1,16 @@
 <template>
   <div>History</div>
-  <container class="grid grid-cols-[56px_1fr]">
+  <container class="grid grid-cols-[56px_1fr] overflow-y-auto max-h-[300px]">
     <div class="font-semibold">
-      <div>Round</div>
+      <div class="firstLine">Round</div>
       <div v-for="(item, index) in store.getRoundNumber" :key="index">
         {{ item }}
       </div>
     </div>
     <div class="overflow-x-scroll">
-      <div class="flex flex-row gap-2 md:justify-evenly alternateBlue">
+      <div class="flex flex-row gap-2 alternateBlue">
         <div v-for="(player, playerId) in store.userList" :key="playerId" class="w-32">
-          <div class="font-semibold">{{ player.name }}</div>
+          <div class="font-semibold firstLine">{{ player.name }}</div>
           <div v-for="roundIScore in player.scorePerRound">
             {{ roundIScore }}
           </div>
@@ -29,5 +29,9 @@ const store = useScoreStore();
 <style scoped>
 .alternateBlue > :nth-child(even) {
   @apply bg-blue-400 text-white px-2;
+}
+.firstLine {
+  /* @apply bg-blue-500 text-white px-2 font-semibold; */
+  /* @apply sticky top-0; */
 }
 </style>
