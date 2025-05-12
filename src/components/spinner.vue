35f08dd1ecaa8383d3373a73868865a41a40d1b5
Ludovic Deparis <maldestor95@gmail.com>
2025-05-12 08:06:18 +0200

    fix spinner rotation

--- src/components/spinner.vue
+++ src/components/spinner.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <svg viewBox="0 0 400 400" class="sector" @click="toggleSpinning">
+    <button @click="spinStart">Start</button>
+    <button @click="spinStop">Stop</button>
+    <svg viewBox="0 0 400 400" class="sector">
       >
       <template v-for="(item, index) in sectors" :key="index">
-        <g :transform="`rotate(${rotationAngle.angle + initialOffset} 200 200)`">
+        <g :transform="`rotate(${rotationAngle.angle} 200 200)`">
           <g :transform="`rotate(${sectorAngle * index} 200 200)`">
             <path
               :d="arc(sectorAngle)"
@@ -16,7 +18,7 @@
         </g>
       </template>
     </svg>
-    <div class="grid grid-cols-2">
+    <div class="grid grid-cols-2 hidden">
       <div>startTime</div>
       <div>{{ startTime }}</div>
       <div>stopTime</div>
@@ -32,9 +34,9 @@
       <div>stopInitiated</div>
       <div>{{ stopInitiated }}</div>
       <div>initialOffset</div>
-      <div>{{ initialOffset }}</div>
+      <div>{{ initialOffset.toFixed(1) }}</div>
       <div>rotationAngle.angle</div>
-      <div>{{ rotationAngle.angle }}</div>
+      <div>{{ rotationAngle.angle.toFixed(1) }}</div>
     </div>
   </div>
 </template>
@@ -48,75 +50,79 @@ interface Props {
 const props = withDefaults(defineProps<Props>(), {
   sectors: () => ["1", "2", "3", "4", "5", "6"],
 });
+
 const sectorAngle = computed(() => {
   return 360 / props.sectors.length;
 });
-const startTime = ref(0);
-const stopTime = ref(0);
+
+const startTime = ref(-1);
+const stopTime = ref(-1);
 const currentTime = ref(0);
 const stopInitiated = ref(false);
 const timer = ref();
 const initialOffset = ref(Math.random() * 360);
+const rotation=ref(initialOffset.value)
 
-const toggleSpinning = () => {
-  if (!stopInitiated.value) {
-    stopTime.value = new Date().getTime();
-    stopInitiated.value = true;
-    const stopSpin= {startTime:startTime.value,stopTime:stopTime.value, rotationAngle:rotationAngle.value, initialOffset:initialOffset.value}
-    console.log('stopSpin', stopSpin)
-    return;
-  }
-    startTime.value = new Date().getTime();
-    const initSpin= {startTime:startTime.value, rotationAngle:initialOffset.value, initialOffset:initialOffset.value}
-    console.log('init Spin', initSpin)
-    stopInitiated.value = false;
-    timer.value = setInterval(() => {
+const spinStart = () => {
+  startTime.value = new Date().getTime();
+  stopInitiated.value = false;
+
+  const initSpin = {
+    startTime: startTime.value,
+    rotationAngle: initialOffset.value,
+    initialOffset: initialOffset.value,
+  };
+  console.log("init Spin", initSpin);
+
+  timer.value = setInterval(() => {
     currentTime.value = new Date().getTime();
   }, 4);
 };
 
+
+const spinStop = () => {
+  if (!stopInitiated.value) { 
+    stopInitiated.value=true
+    stopTime.value = new Date().getTime();
+  }
+};
+
 const rotationAngle = computed(() => {
   currentTime.value = new Date().getTime();
-  const elapsedTime = currentTime.value - startTime.value;
-  const elapsedStopTime = currentTime.value - stopTime.value;
+  const elapsedTime = startTime.value > 0 ? currentTime.value - startTime.value : -1;
+  const elapsedStopTime = stopTime.value > 0 ? currentTime.value - stopTime.value : -1;
   const Vcruise = 90;
   let VV = 0;
+  const T1 = 2500; // increase speed duration
+  const T2 = 4000; // decrease speed duration
 
-  let rotation; // = (elapsedTime / 1000) * 360;
-  const CruiseStartTime = 2500; // 1 second
-  const CruiseStopDuration = 3000; // 1 second
-
-  if (elapsedTime <= CruiseStartTime) {
+  
+  if (elapsedTime <= T1 && elapsedTime > 0) {
     //increase
-    VV = Vcruise * (elapsedTime / CruiseStartTime);
-    rotation = (elapsedTime / 1000) * VV;
-    return { angle: rotation, vitesse: VV };
+    VV = 12*Vcruise * (elapsedTime / T1);
+    rotation.value = rotation.value+ VV/1000;
   }
-  if (elapsedStopTime > CruiseStopDuration && stopInitiated.value) {
+  if (elapsedTime > T1 && stopTime.value == -1) {
+// cruise
+    VV = 3*Vcruise;
+    rotation.value=rotation.value+VV/1000*4
+}
+  if (elapsedStopTime > T2) {
+    // stop
     clearInterval(timer.value);
-    initialOffset.value = rotationAngle.value.angle;
-    stopInitiated.value = false;
-    stopTime.value = 0;
+    startTime.value = -1;
+    stopTime.value = -1;
+    stopInitiated.value=false
   }
 
-  if (currentTime.value > stopTime.value && stopInitiated.value) {
+  if (elapsedStopTime > 0 && elapsedStopTime < T2) {
     //decrease
-    VV = Vcruise * (1 - elapsedStopTime / CruiseStopDuration / 4);
-    rotation = (elapsedTime / 1000) * VV;
-    // console.log("decrease",VV,rotation)
-    return { angle: rotation, vitesse: VV };
+    VV = 12*Vcruise * (1-elapsedStopTime / T2 );
+    rotation.value = rotation.value+ VV/1000;
   }
 
-  if (timer.value && stopInitiated.value == false) {
-    VV = Vcruise;
-    rotation = (elapsedTime / 1000) * VV;
-    return { angle: rotation, vitesse: VV };
-  }
-
-  VV = Vcruise;
-  console.log(Vcruise);
-  rotation = (elapsedTime / 1000) * VV;
-  return { angle: rotation, vitesse: VV };
+  const angle:number=(Number(rotation.value.toFixed(2)) + initialOffset.value)%360
+  return { angle: angle, vitesse: VV };
 });
 
 const arc = (angleInDegree: number) => {
